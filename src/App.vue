cf2fe474bb027c852a3edd02da036fd08565b6b8
guoshuangyang <59650737+guoshuangyang@users.noreply.github.com>
2023-10-18 19:08:23 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { ref } from 'vue';
+const testValue = ref('10px');
+setInterval(() => {
+  testValue.value = Math.random() * 80 + 'px';
+}, 500);
+</script>
+//
+<script>
+// export default {
+//   data() {
+//     return {
+//       testValue1: '20px',
+//     };
+//   },
+//   mounted() {
+//     setInterval(() => {
+//       this.testValue1 = Math.random() * 80 + 'px';
+//     }, 200);
+//   },
+// };
+//
+</script>
+<template>
+  <div
+    :style="{
+      padding: testValue,
+    }"
+  >
+    {{ new Date().toLocaleString() }}
+  </div>
+  <!-- <div
+    :style="{
+      padding: testValue1,
+    }"
+  >
+    {{ new Date().toLocaleString() }}
+  </div> -->
+</template>
